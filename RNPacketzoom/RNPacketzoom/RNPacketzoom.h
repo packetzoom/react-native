43428ab477fe2eb068423846de0b90746adca163
pzreact <leanid@packetzoom.com>
2016-11-10 17:36:31 -0800

    added Xcode project

--- RNPacketzoom/RNPacketzoom/RNPacketzoom.h
+++ RNPacketzoom/RNPacketzoom/RNPacketzoom.h
@@ -7,7 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
 
-@interface RNPacketzoom : NSObject
+@interface RNPacketzoom : NSObject<RCTBridgeModule>
 
 @end
--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 if [ "$(uname)" == "Darwin" ]; then
-unzip -o ./RNSignalStrength/PZSpeed.framework.zip -d RNSignalStrength
+unzip -o ./RNPacketzoom/PZSpeed.framework.zip -d RNSignalStrength
 fi
-#cp -fr chrome-mac /Applications
-#rm -fr chrome-mac chrome-mac.zip
